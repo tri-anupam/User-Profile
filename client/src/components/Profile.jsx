cf47223589afe03e94393dca0e34626b5be393f0
Anupam Tripathi <arvindtri.70@gmail.com>
2023-09-05 19:56:56 +0530

    ui completed

--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -1,22 +1,26 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import addUser from "../assets/add-user.png";
-import styles from "../styles/Username.module.css";
 import { Toaster } from "react-hot-toast";
 import { useFormik } from "formik";
-import { registerValidation } from "../helper/validate.js";
+import { profileValidation } from "../helper/validate.js";
 import convertToBase64 from "../helper/convert";
+
+// import styles from "../styles/Username.module.css";
+// import extend from "../styles/Profile.module.css";
 
 const Profile = () => {
   const [file, setFile] = useState();
 
   const formik = useFormik({
     initialValues: {
+      firstName: "",
+      lastName: "",
       email: "",
-      username: "",
-      password: "",
+      mobile: "",
+      address: "",
     },
-    validate: registerValidation,
+    validate: profileValidation,
     validateOnBlur: false,
     validateOnChange: false,
     onSubmit: async (values) => {
@@ -34,28 +38,26 @@ const Profile = () => {
   return (
     <div className="container mx-auto">
       <Toaster position="top-center" reverseOrder={false}></Toaster>
-      <div className="flex justify-center items-center h-screen ">
+      <div className="flex justify-center items-center h-screen">
         <div
-          className={styles.glass}
-          style={{ width: "45%", height: "fit-content" }}
+          // className={`${styles.glass} ${extend.glass}`}
+          className="border-4 border-gray-50 shrink-0 md:w-[30%] w-[90%] h-fit rounded-3xl  min-w-max shadow-md shadow-[#bbb] bg-[#ffffff61] md:py-4 py-2"
         >
           <div className="title flex flex-col items-center">
-            <h4 className="md:text-5xl text-4xl font-bold font-400">
+            <h4 className="md:text-5xl text-3xl font-bold font-400">Profile</h4>
+            <span className="md:py-4 py-2 md:text-xl text-l w-2/3 text-center text-gray-500">
               You can update the details.
-            </h4>
-            <span className="py-4 md:text-xl text-l w-2/3 text-center text-gray-500">
-              Happy to join you.
             </span>
           </div>
 
-          <form className="py-1" onSubmit={formik.handleSubmit}>
-            <div className="profile flex justify-center py-4">
+          <form onSubmit={formik.handleSubmit}>
+            <div className="profile flex justify-center md:py-4 py-2">
               <label htmlFor="profile">
                 <img
                   src={file || addUser}
                   alt="avatar"
-                  className={styles.profile_img}
-                  style={{ height: "110px", width: "110px" }}
+                  // className={`${styles.profile_img} ${extend.profile_img}`}
+                  className="border-4 border-gray-100 rounded-full shadow-lg cursor-pointer hover:border-gray-200 md:h-40 md:w-40 h-24 w-24"
                 />
               </label>
               <input
@@ -67,34 +69,38 @@ const Profile = () => {
               />
             </div>
 
-            <div className="textbox flex flex-col items-center gap-6">
-              <div className="name flex w-3/4 gap-10">
+            <div className="textbox flex flex-col items-center justify-center md:gap-6 gap-3">
+              <div className="name flex items-center md:w-3/4 w-[90%] md:gap-10 gap-4 flex-col md:flex-row">
                 <input
                   {...formik.getFieldProps("firstName")}
                   type="text"
                   placeholder="First Name"
-                  className={styles.textbox}
+                  // className={`${styles.textbox} ${extend.textbox}`}
+                  className="border-0 px-3 py-3 rounded-xl md:w-3/4  w-[90%] shadow-sm text-lg focus:outline-none"
                 />
                 <input
                   {...formik.getFieldProps("lastName")}
                   type="text"
                   placeholder="Last Name"
-                  className={styles.textbox}
+                  // className={`${styles.textbox} ${extend.textbox}`}
+                  className="border-0 px-3 py-3 rounded-xl md:w-3/4  w-[90%] shadow-sm text-lg focus:outline-none"
                 />
               </div>
 
-              <div className="name flex w-3/4 gap-10">
+              <div className="name flex items-center md:w-3/4 w-[90%] md:gap-10 gap-4  flex-col md:flex-row">
                 <input
                   {...formik.getFieldProps("mobile")}
                   type="text"
                   placeholder="Mobile No."
-                  className={styles.textbox}
+                  // className={`${styles.textbox} ${extend.textbox}`}
+                  className="border-0 px-3 py-3 rounded-xl md:w-3/4  w-[90%] shadow-sm text-lg focus:outline-none"
                 />
                 <input
                   {...formik.getFieldProps("email")}
                   type="email"
                   placeholder="Email"
-                  className={styles.textbox}
+                  // className={`${styles.textbox} ${extend.textbox}`}
+                  className="border-0 px-3 py-3 rounded-xl md:w-3/4  w-[90%] shadow-sm text-lg focus:outline-none"
                 />
               </div>
 
@@ -102,10 +108,16 @@ const Profile = () => {
                 {...formik.getFieldProps("address")}
                 type="text"
                 placeholder="Address"
-                className={styles.textbox}
+                // className={`${styles.textbox} ${extend.textbox}`}
+                className="border-0 px-3 py-3 rounded-xl md:w-3/4  w-[82%] shadow-sm text-lg focus:outline-none"
               />
 
-              <button className={styles.btn} type="submit">
+              <button
+                //  className={styles.btn}
+                className="border border-gray-500 md:w-3/4 w-[90%] py-3 rounded-lg text-gray-600 [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none
+                text-xl shadow-sm text-center hover:bg-[#ff6a6a] hover:text-white hover:border-white"
+                type="submit"
+              >
                 Update
               </button>
             </div>
--- client/src/helper/validate.js
+++ client/src/helper/validate.js
@@ -21,7 +21,15 @@ export async function resetPasswordVaidate(values) {
 
 //validate register form
 export async function registerValidation(values) {
-  const errors = registerUser({}, values);
+  const errors = usernameVerify({}, values);
+  passwordVerify(errors, values);
+  emailVerify(errors, values);
+  return errors;
+}
+
+//validate profile page
+export async function profileValidation(values) {
+  const errors = emailVerify({}, values);
   return errors;
 }
 
@@ -63,28 +71,8 @@ function resetPasswordVerify(error = {}, values) {
   return error;
 }
 
-//register validate
-function registerUser(errors = {}, values) {
-  //password
-  const specialChars = /[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
-  if (!values.password) {
-    errors.password = toast.error("Password Required");
-  } else if (values.password.includes(" ")) {
-    errors.password = toast.error("Invalid Password");
-  } else if (values.password.length < 4) {
-    errors.password = toast.error("Password should be max to 4 characters");
-  } else if (!specialChars.test(values.password)) {
-    errors.password = toast.error(
-      "Password should contain special characters and numbers"
-    );
-  }
-  //username
-  if (!values.username) {
-    errors.username = toast.error("Username Required");
-  } else if (values.username.includes(" ")) {
-    errors.username = toast.error("Invalid Username");
-  }
-  //email
+//email verify
+function emailVerify(errors = {}, values) {
   if (!values.email) {
     errors.email = toast.error("Email Required");
   } else if (values.email.includes(" ")) {
